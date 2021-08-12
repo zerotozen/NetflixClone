df7817ca8c26e3c78d345f4e296504ec1d30c952
zerozen <zerozen@tutamail.com>
2021-08-12 18:30:03 +0200

    update video player

--- src/containers/browse.js
+++ src/containers/browse.js
@@ -116,7 +116,7 @@ export function BrowseContainer({ slides }) {
             <Card.Feature category={category}>
               <Player>
                 <Player.Button />
-                <Player.Video src="/videos/bunny.mp4" />
+                <Player.Video src="/videos/netflix-video.mp4" />
               </Player>
             </Card.Feature>
           </Card>
